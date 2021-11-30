859024ddde1436743d3effc11c52a86baf49a90f
Alex <kosianlucky@mail.ru>
2021-11-30 19:30:35 +0300

    making up text and picture field plugin AFC code PHP

--- functions.php
+++ functions.php
@@ -4,5 +4,9 @@
     function childhood_scripts(){
         wp_enqueue_style('childhood-style', get_stylesheet_uri());
         wp_enqueue_script('childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
-    };
+
+        
+    }
+
+    add_theme_support( 'custom-logo' );
 ?>
